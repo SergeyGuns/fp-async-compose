9162d5bdbc7ba9cd355d8244d136d81532bce739
Sergey Ganin <mcheguevara2@gmail.com>
2019-12-30 16:08:27 +0300

    first

---
+++ index.js
@@ -0,0 +1,55 @@
+const composeAsync = (...fns)=> function(...args) {
+  let result = null;
+  let err = null;
+  try {
+    console.log(fns[0].name, {args})
+    fns[0](...args , (e, res) => {
+      if(e) return err=e;
+      return result = res;
+		})
+  } catch (e) {
+    return err = e
+  }
+  function next() {
+    if(result) {                            
+      const [_,...tailFns] = fns                
+      return tailFns !== undefined ?               
+      composeAsync(...tailFns)(result):result                                    
+    }
+    if(err) console.log(err);
+    if(result === null && err === null) {
+      setTimeout(next, 0)
+    }                                     
+  }
+  next()
+}
+
+function asyncFunc(time,fnc){
+  return function (payload, callback){
+    setTimeout(()=>fnc(payload, callback)
+    ,time)
+  }
+}
+
+const log = (payload,callback) => {
+  callback(null,payload)
+}
+
+const increment = (payload, callback)=> {
+  callback(null,payload+1)
+}
+
+const decrement = (payload, callback)=> {
+  callback(null,payload-1)
+}
+
+composeAsync(
+  log,
+  asyncFunc(1000, increment), 
+  log, 
+  increment,
+  asyncFunc(1000, decrement),
+  log
+)(1)
+
+
